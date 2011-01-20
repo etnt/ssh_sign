e136a0cb981357a92ea38c2b4fbfd65f2caf49c5
Carl-Johan Kjellander <carl-johan@klarna.com>
2011-01-20 13:46:31 +0100

    whitespace

--- src/ssh_sign.erl
+++ src/ssh_sign.erl
@@ -2,7 +2,7 @@
 %%% Created :  7 Dec 2010 by Torbjorn Tornkvist <tobbe@tornkvist.org>
 %%%
 %%% @doc Do Sign and Verify of data, making use of the users SSH keys.
-%%%      This code is just enabling some missing functionality in the 
+%%%      This code is just enabling some missing functionality in the
 %%%      R1304 ssh app.
 %%% @end
 %%%-------------------------------------------------------------------
@@ -31,7 +31,7 @@ sign(Data) when is_binary(Data) ->
 
 verify(Data, Sig) when is_binary(Data), is_binary(Sig) ->
     {ok,Key} = public_identity_key("ssh-rsa",[]),
-    ssh_rsa:verify(Key, Data, Sig).    
+    ssh_rsa:verify(Key, Data, Sig).
 
 public_identity_key(Alg, Opts) ->
     Path = ssh_file:file_name(user, public_identity_key_filename(Alg), Opts),
@@ -43,7 +43,7 @@ public_identity_key_filename("ssh-rsa") -> "id_rsa.pub".
 read_public_key_v2(File, Type) ->
     case file:read_file(File) of
         {ok,Bin} ->
-            List = binary_to_list(Bin),            
+            List = binary_to_list(Bin),
             case lists:prefix(Type, List) of
                 true ->
                     List1 = lists:nthtail(length(Type), List),
